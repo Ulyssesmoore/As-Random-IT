dab1ad86cf91a0275d64b4fe7bec29e8e576e0a5
Ulysses Moore <sander.debruin5c@gmail.com>
2016-05-28 13:36:44 +0200

    Start application

---
+++ application/views/pages/home.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<!-- header.php -->
+    <title>Home | As Random IT</title>
+  </head>
+  <body>
+    <h1>My second header</h1>
+  </body>
+</html>
---
+++ application/views/templates/header.php
@@ -0,0 +1,6 @@
+<html>
+  <head>
+    <meta charset="UTF-8"/>
+    <link rel="stylesheet" href="<?php echo base_url('/resources/styles/skeleton/css/normalize.css');?>"/>
+    <link rel="stylesheet" href="<?php echo base_url('/resources/styles/skeleton/css/skeleton.css');?>"/>
+    <link rel="stylesheet" href="<?php echo base_url('/resources/styles/main.css');?>"
