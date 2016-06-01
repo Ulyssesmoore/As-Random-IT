954e18aa5ed6329446d1142639c287a849b92ee3
Ulysses Moore <sander.debruin5c@gmail.com>
2016-06-01 12:55:08 +0200

    Progressie CI Test

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -3,6 +3,27 @@
     <title>Home | As Random IT</title>
   </head>
   <body>
-    <h1>My second header</h1>
-  </body>
-</html>
+    <nav>
+      <ul>
+        <li>
+          <a href="/mysite"><span class="icon icon-home"></span>Home</a>
+        </li><li>
+          <a href="/mysite/index.php/about">About</a>
+        </li>
+      </ul>
+    </nav>
+    <div class="container">
+      <div class= "flexwrapper">
+        <div class="center">
+          <h2 class="title">Welcome to As Random IT</h2>
+        </div>
+      </div>
+      <div class="paragraph">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ex leo, ullamcorper vitae consequat accumsan, volutpat eget est. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur mollis tristique arcu, vulputate condimentum nisl feugiat vitae. Mauris cursus iaculis justo. Nullam ac sapien sed lorem volutpat consequat at in elit. Vivamus ullamcorper congue metus a congue. Nulla eu libero egestas, dapibus nisi vel, aliquam mi. Mauris facilisis rhoncus aliquam. Integer facilisis arcu justo, sit amet convallis libero consectetur ac. Nulla a egestas nunc. Curabitur velit lectus, fermentum et leo a, finibus dapibus nulla. Curabitur pulvinar commodo lorem, nec feugiat tellus convallis sed. Aenean urna sapien, auctor vel rutrum et, sodales quis mi. Fusce in aliquet felis. Sed ullamcorper facilisis elementum. Phasellus fringilla ex eu ex dictum ultricies.</p>
+        <p>Phasellus vitae tellus a magna porta ullamcorper vel vitae neque. Duis fermentum massa sit amet nunc convallis, at elementum augue facilisis. Sed pretium semper est, in aliquam metus pretium vitae. Sed ac nisl quis elit imperdiet placerat quis at nibh. Aenean dictum vulputate nunc nec eleifend. Proin ut auctor libero. Vestibulum ac hendrerit ipsum, id blandit augue. Proin orci tellus, malesuada ut fringilla porttitor, commodo eget nisi. Pellentesque hendrerit at velit ut pretium. Vestibulum aliquam convallis dolor id consequat. Duis accumsan finibus condimentum.</p>
+        <p>Aliquam sit amet scelerisque urna, in blandit arcu. Proin diam ante, facilisis et pharetra consectetur, feugiat eu augue. Aliquam erat volutpat. Nunc eleifend placerat ante, sit amet faucibus sem molestie eget. Praesent sed nibh imperdiet, rutrum lacus eget, accumsan neque. Nam in sem ligula. Nullam fermentum venenatis laoreet. Suspendisse cursus quam non justo dignissim luctus. Donec commodo orci a egestas ullamcorper.</p>
+        <p>Sed id arcu libero. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec at odio ultrices, luctus tellus sit amet, vehicula dolor. Nulla venenatis arcu nulla, eu pulvinar dolor hendrerit sit amet. Aenean sodales tincidunt eros, non finibus neque posuere a. Sed ullamcorper, lacus eu placerat luctus, lorem neque cursus lacus, a pretium nunc mauris ut sapien. Quisque sit amet porta odio, at dapibus orci. Proin sed est eu nulla ullamcorper vestibulum.</p>
+        <p>Morbi sollicitudin velit fringilla ipsum pulvinar, id congue arcu elementum. Cras tellus nisi, venenatis in ligula id, euismod dapibus urna. Nunc vehicula tempor arcu at venenatis. Vivamus commodo neque eget dui condimentum cursus. Nulla rhoncus ornare egestas. Nulla blandit mollis semper. Phasellus lobortis nibh risus, nec suscipit lectus euismod sed. Fusce sit amet rutrum nulla. Proin dapibus, lectus nec luctus mattis, purus quam pretium enim, at hendrerit quam leo quis ex. Pellentesque congue felis eros, vel varius ligula iaculis at. Donec iaculis purus id sapien dictum consectetur condimentum a dui. Proin sed risus tincidunt, sollicitudin magna id, congue massa. Nunc consectetur ante non venenatis interdum. Aenean fermentum vulputate tincidunt. Nulla molestie ante id libero pretium porta.</p>
+      </div>
+    </div>
+<!-- footer.php -->
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,6 +1,14 @@
+<!DOCTYPE html>
 <html>
   <head>
     <meta charset="UTF-8"/>
-    <link rel="stylesheet" href="<?php echo base_url('/resources/styles/skeleton/css/normalize.css');?>"/>
+    <!-- CSS -->
+    <link rel="stylesheet" href="<?php echo base_url();?>/resources/styles/skeleton/css/normalize.css"/>
+    <!-- <link rel="stylesheet" href="/resources/styles/skeleton/css/normalize.css"/> -->
     <link rel="stylesheet" href="<?php echo base_url('/resources/styles/skeleton/css/skeleton.css');?>"/>
-    <link rel="stylesheet" href="<?php echo base_url('/resources/styles/main.css');?>"
+    <!-- <link rel="stylesheet" href="/resources/styles/skeleton/css/skeleton.css"/> -->
+    <link rel="stylesheet" href="<?php echo base_url('/resources/styles/main.css');?>"/>
+    <!-- <link rel="stylesheet" href="/resources/styles/main.css"/> -->
+    <link rel="stylesheet" href="<?php echo base_url('/resources/styles/font-awesome.min.css'); ?>" />
+    <!-- JavaScript -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
